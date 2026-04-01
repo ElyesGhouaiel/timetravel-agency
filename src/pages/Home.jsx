836eb5d9ac4985647e07ca30941f5023a0655035
Ghouaiel Elyes <elyes06700@gmail.com>
2026-04-01 15:33:47 +0200

    assemble home page layout with all sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import DestinationsGrid from '../components/DestinationsGrid'
+import ChatWidget from '../components/ChatWidget'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
@@ -7,7 +10,16 @@ export default function Home() {
       <Navbar />
       <main>
         <Hero />
+        <section id="destinations" style={{
+          padding: 'var(--space-24) var(--space-6)',
+          maxWidth: '1200px',
+          margin: '0 auto',
+        }}>
+          <DestinationsGrid />
+        </section>
       </main>
+      <ChatWidget />
+      <Footer />
     </>
   )
 }
