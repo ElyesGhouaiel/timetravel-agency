a39ba4209d8d3200ec4a460559a6868383b3652d
killian <killianc142@gmail.com>
2026-04-01 17:18:32 +0200

    Mise en place du quiz + élément additionnel des card et grid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import DestinationsGrid from '../components/DestinationsGrid'
 import ChatWidget from '../components/ChatWidget'
 import Footer from '../components/Footer'
+import PersonalityQuiz from '../components/PersonalityQuiz'
 
 export default function Home() {
   return (
@@ -19,6 +20,16 @@ export default function Home() {
           <DestinationsGrid />
         </section>
       </main>
+      <section
+  id="quiz"
+  style={{
+    padding: 'var(--space-24) var(--space-6)',
+    maxWidth: '1200px',
+    margin: '0 auto',
+  }}
+>
+  <PersonalityQuiz />
+</section>
       <ChatWidget />
       <Footer />
     </>
